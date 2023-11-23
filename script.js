d3f0dbf287c334a7a2ca81f4302877a8091f8a51
LintangArsya <148044892+LintangArsya@users.noreply.github.com>
2023-11-23 19:22:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+Math.round
+let a = 5.2;
+let b = 5.6;
+let c = 5.5;
+Math.floor
+let d = 7.2;
+let e = 9.6;
+let f = 8.9;
+Math.ceil
+let g = 7.2;
+let h = 9.3;
+let i = 8.1;
+Math.pow
+let j = 2;
+let k = 4;
+let l = 5;
+let j1 = 2;
+let k2 = 4;
+let l3 = 3;
+Math.sqrt
+let m = 36;
+let n = 81;
+let o = 121;
+
+let round = Math.round(a);
+console.log(round);
+let round1 = Math.round(b);
+console.log(round1);
+let round2 = Math.round(c);
+console.log(round2);
+
+let floor = Math.floor(d);
+console.log(floor);
+let floor1 = Math.floor(e);
+console.log(floor1);
+let floor2 = Math.floor(f);
+console.log(floor2);
+
+let ceil = Math.ceil(g)
+console.log(ceil)
+let ceil1 = Math.ceil(h)
+console.log(ceil1)
+let ceil2 = Math.ceil(i)
+console.log(ceil2)
+
+let pow = Math.pow(j,j1)
+console.log(pow)
+let pow1 = Math.pow(k,k2)
+console.log(pow1)
+let pow2 = Math.pow(l,l3)
+console.log(pow2)
+
+let sqrt = Math.sqrt(m)
+console.log(sqrt)
+let sqrt1 = Math.sqrt(n)
+console.log(sqrt1)
+let sqrt2 = Math.sqrt(o)
+console.log(sqrt2)
